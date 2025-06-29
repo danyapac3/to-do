e7b7433d49e87cc2e31228c030bbb06934fc9c2f
danya.pac3 <danya.pac3@gmail.com>
2025-06-29 08:39:37 +0300

    Add button for adding section but without behaviour

--- src/js/components/board.js
+++ src/js/components/board.js
@@ -2,6 +2,7 @@ import Component from '/js/lib/component';
 import {htmlToNode} from '/js/lib/utils/dom';
 import template from './board.html';
 import List from './list';
+import AddItem from './add-item';
 
 export default class Board extends Component {
 
@@ -19,6 +20,7 @@ export default class Board extends Component {
     const title = board.querySelector('.board__title');
     const project = this.store.state.projects.find(p => p.id === this.store.state.currentProjectId);
     const boardContent = board.querySelector('.board__content');
+    const addNewSectionFormPlace = board.querySelector('.board__add-new-section-form-place');
 
     if (project && project.listIds) {
       for (let listId of project.listIds) {
@@ -29,6 +31,12 @@ export default class Board extends Component {
         boardContent.append(list.element);
       }
     }
+
+    const addSectionForm = new AddItem({
+      parent: this,
+      props: {title: 'Add new section'}
+    });
+    addNewSectionFormPlace.appendChild(addSectionForm.element);
 
     if (project && project.title) {
       title.textContent = project.title;
