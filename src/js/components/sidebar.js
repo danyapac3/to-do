6a086e06dc0851c00329eb24080b87627c3a52ed
danya.pac3 <danya.pac3@gmail.com>
2025-08-09 20:28:49 +0300

    Add event delegation for all events on sidebar

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -4,29 +4,43 @@ import template from './sidebar.html';
 import useProjectsStore from "/js/stores/projectsStore";
 import useListsStore from "/js/stores/listsStore";
 
+const projectsStore = useProjectsStore();
+
+
+const matchProjectElm = (elm) => elm.classList.contains('sidebar__item--project');
 
 const createProject = (project) => {
   const element = document.createElement('div');
 
   element.classList.add('sidebar__item',  'sidebar__item--project');
   element.dataset.id = project.id;
-  if (project.hue) {
-    element.style.setProperty('--hue', project.hue);
-  }
+  if (project.hue) element.style.setProperty('--hue', project.hue);
   element.textContent = project.title;
   return element;
 }
+
 
 export default class Sidebar extends Component {
   constructor({parent}) {
     super({
       parent,
       element: htmlToNode(template),
+      stores: [useProjectsStore()],
     });
+  }
+
+  renderPredicate() {
+    return false;
   }
 
   init() {
     const $sidebar = this.element;
+    const $todayButton = $sidebar.querySelector('.sidebar__item[data-filtered-for="today"]');
+
+    $todayButton.addEventListener('click', () => {
+      this.emit('selectToday');
+    });
+
     $sidebar.addEventListener('click', ({target}) => {
       if (![...target.classList].includes('sidebar__item')) return;
       const item = target;
@@ -34,58 +48,58 @@ export default class Sidebar extends Component {
         elm.classList.remove('sidebar__item--active');
       });
       item.classList.add('sidebar__item--active');
-    }) 
-  }
-
-  render() {
-    const $sidebar = this.element;
-    const $toggleVisibilityButton = this.element.querySelector('.sidebar__toggle-visibility-button');
-    const $sidebarProjects = $sidebar.querySelector('.sidebar__section[data-type="projects"]');
-    const $sidebarProjectsContent = $sidebarProjects.querySelector('.sidebar__section-content');
-    const $todayButton = $sidebar.querySelector('.sidebar__item[data-filtered-for="today"]');
-
-    $todayButton.addEventListener('click', () => {
-      this.emit('selectToday');
     });
 
+    const $toggleVisibilityButton = this.element.querySelector('.sidebar__toggle-visibility-button');
     $toggleVisibilityButton.addEventListener('click', () => {
       $sidebar.classList.toggle('collapsed');
     });
 
-    const projectsStore = useProjectsStore();
-    const projectElements = Object.values(projectsStore.$state)
-      .map(project => {
-        const elm = createProject(project);
-        elm.addEventListener('click', () => {
-          this.emit('selectProject', {id: project.id});
-        });
-        elm.addEventListener('dragenter', (e) => {
-          elm.classList.add('drag-over'); 
-        });
-        elm.addEventListener('dragleave', (e) => {
-          elm.classList.remove('drag-over'); 
-        });        
-        elm.addEventListener('dragover', (e) => {
-          e.preventDefault();
-        });
-        elm.addEventListener('drop', (e) => {
-          elm.classList.remove('drag-over'); 
-          let data;
-          
-          try {
-            data = JSON.parse(e.dataTransfer.getData('Text'));
-          } catch (e) {
-            return;
-          }
+    $sidebar.addEventListener('dragover', (e) => {
+      e.preventDefault();
+    });
 
-          if (data.type === 'list') {
-            const list = useListsStore().$state[data.id];
+    $sidebar.addEventListener('dragenter', ({target}) => {
+      if (!matchProjectElm(target)) return;
+      target.classList.add('drag-over');
+    });
 
-            useProjectsStore().moveListToProject(list.id, list.parentId, project.id);
-          }
-        });
-        return elm;
-      });
+    $sidebar.addEventListener('click', ({target}) => {
+      if (!matchProjectElm(target)) return;
+      this.emit('selectProject', {id: target.dataset.id});
+    });
+
+    $sidebar.addEventListener('dragleave', ({target}) => {
+      if (!matchProjectElm(target)) return;
+      target.classList.remove('drag-over');
+    });
+
+    $sidebar.addEventListener('drop', ({target, dataTransfer}) => {
+      if (!matchProjectElm(target)) return;
+      
+      target.classList.remove('drag-over'); 
+      
+      let data;
+      try { data = JSON.parse(dataTransfer.getData('Text')); }
+      catch { return }
+
+      if (data.type === 'list') {
+        const list = useListsStore().$state[data.id];
+
+        useProjectsStore().moveListToProject(list.id, list.parentId, target.dataset.id);
+      }
+    });
+  }
+
+  render() {
+    const $sidebar = this.element;
+    const $sidebarProjects = $sidebar.querySelector('.sidebar__section[data-type="projects"]');
+    const $sidebarProjectsContent = $sidebarProjects.querySelector('.sidebar__section-content');
+
+    const projectElements = Object.values(projectsStore.$state).map(project => {
+      const elm = createProject(project);
+      return elm;
+    });
 
     $sidebarProjectsContent.replaceChildren(...projectElements);
   }
---
+++ src/js/stores/appStore.js
@@ -0,0 +1,26 @@
+import defineStore from "/js/lib/store";
+import useProjectsStore from "./projectsStore";
+
+const useAppStore = defineStore({
+  state: {
+    currentProject: "system.today",
+  },
+
+  actions: {
+    setCurrentBoard(id) {
+      const projectsStore = useProjectsStore();
+      const possibleIds = [
+        'system.today',
+        'system.upcoming',
+        'system.inbox',
+        ...Object.keys(projectsStore.$state),
+      ];
+      if (!possibleIds.includes(id)) {
+        throw new Error(`There is no such project with id: ${id}`);
+      }
+      this.currentProject = id;
+    }
+  },
+});
+
+export default useAppStore;
