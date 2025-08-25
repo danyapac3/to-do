700509e027864535ab25441cfb88a609aae03656
danya.pac3 <danya.pac3@gmail.com>
2025-08-25 19:01:05 +0300

    Deployment commit

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import "/styles/index.scss";
-
-import storage from '/js/lib/storage';
-import Page from '/js/components/page';
-import Sortable, { AutoScroll } from 'sortablejs/modular/sortable.core.esm';
-
-Sortable.mount( new AutoScroll );
-const pageElement = document.querySelector('.page');
-const page = new Page({element: pageElement, parent: null});
