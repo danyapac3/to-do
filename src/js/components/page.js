f728a0a88162629456c61c8384f95bc8394efdbe
danya.pac3 <danya.pac3@gmail.com>
2025-08-25 16:58:53 +0300

    Improve sidebar and make it responsive

--- src/js/components/page.js
+++ src/js/components/page.js
@@ -13,13 +13,10 @@ export default class Page extends Component {
 
   render() {
     const $page = this.element;
-    const $sidebarPlace = $page.querySelector('.page__sidebar-place');
-    
     const sidebar = new Sidebar({ parent: this });
-    $sidebarPlace.appendChild(sidebar.element);
-
     const appContent = new AppContent({ parent: this });
     appContent.element.classList.add('page__content');
-    $page.appendChild(appContent.element); 
+    $page.appendChild(sidebar.element);
+    $page.appendChild(appContent.element);
   }
 }
